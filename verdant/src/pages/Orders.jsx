f080b8fad10c1e2e6ef2642f80937ce6b4e54204
harithas1 <163287893+harithas1@users.noreply.github.com>
2025-04-19 09:54:23 +0530

    corrected  module name

--- verdant/src/pages/Orders.jsx
+++ verdant/src/pages/Orders.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
+import { toast } from "sonner";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
